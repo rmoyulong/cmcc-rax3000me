e4e4a9253338eaab653341ddbb1dad6c82fd70e7
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-06-25 23:17:36 +0800

    cmcc rax3000me

--- patch/mediatek.sh
+++ patch/mediatek.sh
@@ -1,5 +1,6 @@
 cd openwrt
 
+mv $GITHUB_WORKSPACE/patch/mt7981/Makefile target/linux/mediatek/Makefile
 cp -rf $GITHUB_WORKSPACE/patch/mt7981/filogic.mk target/linux/mediatek/image/filogic.mk
 cp -rf $GITHUB_WORKSPACE/patch/mt7981/mt7981b-cmcc-rax3000me.dts target/linux/mediatek/dts/mt7981b-cmcc-rax3000me.dts
 ls -l target/linux/mediatek
